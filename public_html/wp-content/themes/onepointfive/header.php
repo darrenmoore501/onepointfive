48a05f3872f74899fe2feb732fbf388681cc1cc3
Darren Moore <darren@dorsetroad.co.uk>
2020-02-07 15:01:58 +0000

    mobile menu

--- public_html/wp-content/themes/onepointfive/header.php
+++ public_html/wp-content/themes/onepointfive/header.php
@@ -28,7 +28,10 @@
 	<meta name="theme-color" content="#F44336">
 	<meta name="msapplication-navbutton-color" content="#F44336">
 	<meta name="apple-mobile-web-app-status-bar-style" content="#F44336">
-
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 <link href="https://fonts.googleapis.com/css?family=Cabin&display=swap" rel="stylesheet">

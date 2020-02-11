fb14191e359ff3bade29ab3948ee74aeb6313c14
Darren Moore <darren@dorsetroad.co.uk>
2020-02-11 17:49:35 +0000

    added company number

--- public_html/wp-content/themes/onepointfive/template-parts/footer/layout-address.php
+++ public_html/wp-content/themes/onepointfive/template-parts/footer/layout-address.php
@@ -1,6 +1,6 @@
 <div class="col">
 	<div class="address">
-		<p><strong>One Point Five Comms Ltd</strong></p>
+		<p><strong>One Point Five Comms Ltd</strong> <br />(Company Number 12448317)</p>
 		<p>The Workary, 1 Dukes Ave, Chiswick, W4 2AB, UK.</p>
 	</div>
 </div>
